607de1ed7d9a33eee58c1a25e51d2db545ce2365
Sergio Guachalla <sergioguachalla0@gmail.com>
2024-04-18 19:06:47 -0400

    first commit

---
+++ src/main/kotlin/com/notification/msnotification/MsNotificationApplication.kt
@@ -0,0 +1,11 @@
+package com.notification.msnotification
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsNotificationApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsNotificationApplication>(*args)
+}
---
+++ src/test/kotlin/com/notification/msnotification/MsNotificationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.notification.msnotification
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsNotificationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
